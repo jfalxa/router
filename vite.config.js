e218b09fe03e8feb771613e6c089187a2b82d02d
Julien Falxa <jfalxa.sta@gmail.com>
2024-10-20 18:56:26 +0200

    add types and fix refactor to accomodate

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
 
   build: {
+    target: "esnext",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
