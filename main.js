4783bfa0461503436eb31e0b0f0df6d9723e7507
Julien Falxa <jfalxa.sta@gmail.com>
2024-10-21 16:20:20 +0200

    remove vite and simplify library access

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// @TODO
+// @ts-ignore: Property 'UrlPattern' does not exist
+if (!globalThis.URLPattern) {
+  await import("urlpattern-polyfill");
+}
+
+export * from "./lib/router";
+export * from "./lib/routes";
+export * from "./lib/route";
+export * from "./lib/task";
